fa0b7a326ec2a5420171d704f64075b67f329e35
valvava7 <william01180118@gmail.com>
2025-11-19 22:12:51 +0800

    fix bug

--- nnunetv2/training/nnUNetTrainer/variants/exp/nnUNetTrainer_opt_nm250.py
+++ nnunetv2/training/nnUNetTrainer/variants/exp/nnUNetTrainer_opt_nm250.py
@@ -20,12 +20,12 @@ class nnUNetTrainer_1en3_nm250(nnUNetTrainer_250epochs_NoMirroring):
         super().__init__(plans, configuration, fold, dataset_json, device)
         self.initial_lr=1e-3
 
-class nnUNetTrainer_adam1en3_nm250_wd0():
+class nnUNetTrainer_adam1en3_nm250_wd0(nnUNetTrainer_250epochs_NoMirroring):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, device: torch.device = torch.device('cuda')):
         super().__init__(plans, configuration, fold, dataset_json, device)
         self.weight_decay = 0
 
-class nnUNetTrainer_adam1en3_nm250_wd3en6():
+class nnUNetTrainer_adam1en3_nm250_wd3en6(nnUNetTrainer_250epochs_NoMirroring):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, device: torch.device = torch.device('cuda')):
         super().__init__(plans, configuration, fold, dataset_json, device)
         self.weight_decay = 3e-6
