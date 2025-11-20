8546105d558eb3c6c036e737c4c37c042f99e01a
valvava7 <william01180118@gmail.com>
2025-11-21 00:26:42 +0800

    more exp

--- nnunetv2/training/nnUNetTrainer/variants/exp/nnUNetTrainer_opt_nm250.py
+++ nnunetv2/training/nnUNetTrainer/variants/exp/nnUNetTrainer_opt_nm250.py
@@ -1,7 +1,7 @@
 import torch
 from nnunetv2.training.nnUNetTrainer.variants.training_length.nnUNetTrainer_Xepochs_NoMirroring import nnUNetTrainer_250epochs_NoMirroring
 from nnunetv2.training.nnUNetTrainer.variants.optimizer.nnUNetTrainerAdam import nnUNetTrainerVanillaAdam, nnUNetTrainerAdam1en3, nnUNetTrainerAdam3en4
-from nnunetv2.training.nnUNetTrainer.variants.optimizer.nnUNetTrainerAdan import nnUNetTrainerAdan1en3
+from nnunetv2.training.nnUNetTrainer.variants.optimizer.nnUNetTrainerAdan import nnUNetTrainerAdan1en3, nnUNetTrainerAdan3en4
 
 class nnUNetTrainer_vadam_nm250(nnUNetTrainer_250epochs_NoMirroring, nnUNetTrainerVanillaAdam):
     pass
@@ -10,6 +10,9 @@ class nnUNetTrainer_adam1en3_nm250(nnUNetTrainer_250epochs_NoMirroring, nnUNetTr
     pass
 
 class nnUNetTrainer_adan1en3_nm250(nnUNetTrainer_250epochs_NoMirroring, nnUNetTrainerAdan1en3):
+    pass
+
+class nnUNetTrainer_adan3en4_nm250(nnUNetTrainer_250epochs_NoMirroring, nnUNetTrainerAdan3en4):
     pass
 
 class nnUNetTrainer_adam3en4_nm250(nnUNetTrainer_250epochs_NoMirroring, nnUNetTrainerAdam3en4):
