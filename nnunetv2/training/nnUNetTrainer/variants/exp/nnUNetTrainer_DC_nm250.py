0c86585a4f9cdbfa79ed8c31333e72abaaa93692
valvava7 <william01180118@gmail.com>
2025-11-30 05:03:14 +0800

    trainer

--- nnunetv2/training/nnUNetTrainer/variants/exp/nnUNetTrainer_DC_nm250.py
+++ nnunetv2/training/nnUNetTrainer/variants/exp/nnUNetTrainer_DC_nm250.py
@@ -7,6 +7,7 @@ from nnunetv2.training.loss.dice import MemoryEfficientSoftDiceLoss
 from nnunetv2.training.nnUNetTrainer.variants.training_length.nnUNetTrainer_Xepochs_NoMirroring import nnUNetTrainer_250epochs_NoMirroring
 from nnunetv2.training.nnUNetTrainer.variants.data_augmentation.nnUNetTrainerNoMirroring import nnUNetTrainerNoMirroring
 from nnunetv2.training.nnUNetTrainer.variants.exp.nnUNetTrainer_opt_nm250 import nnUNetTrainer_adam1en3_nm250
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 from nnunetv2.utilities.helpers import softmax_helper_dim1
 
 
@@ -182,4 +183,26 @@ class nnUNetTRainer_DC0CE(nnUNetTrainerNoMirroring):
         return loss
 
 
+class nnUNetTRainer_DC0CE(nnUNetTrainer):
+    def _build_loss(self):
+        # set smooth to 0
+        if self.label_manager.has_regions:
+            raise NotImplemented
+        loss = DC_and_CE_loss({'batch_dice': self.configuration_manager.batch_dice,
+                                'smooth': 0.0, 'do_bg': False, 'ddp': self.is_ddp}, {}, weight_ce=1, weight_dice=1,
+                                ignore_label=self.label_manager.ignore_label,
+                                dice_class=MemoryEfficientSoftDiceLoss)
 
+        if self.enable_deep_supervision:
+            deep_supervision_scales = self._get_deep_supervision_scales()
+
+            # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
+            # this gives higher resolution outputs more weight in the loss
+            weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+            weights[-1] = 0
+
+            # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
+            weights = weights / weights.sum()
+            # now wrap the loss
+            loss = DeepSupervisionWrapper(loss, weights)
+        return loss
